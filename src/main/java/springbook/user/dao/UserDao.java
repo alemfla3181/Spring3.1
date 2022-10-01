3ab5c4dbfe651447c6ba58d9767108065853b8ce
Dragon <alemfla3181@naver.com>
2022-10-01 17:05:02 +0900

    spring3.1 3장 템플릿

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -114,4 +114,20 @@ public class UserDao {
         }
     }
 
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try{
+            c = dataSource.getConnection();
+
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        } catch (SQLException e){
+            throw e;
+        } finally {
+            if(ps != null){ try {ps.close();} catch (SQLException e){} }
+            if(c != null){ try {c.close();} catch (SQLException e){} }
+        }
+    }
 }
