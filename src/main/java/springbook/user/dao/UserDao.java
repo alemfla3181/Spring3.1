1368689798890e049666f2e9247c1d1510e354d6
Dragon <alemfla3181@naver.com>
2022-10-02 14:00:47 +0900

    spring3.1 3장 템플릿 익명 내부 클래스

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -13,32 +13,42 @@ public class UserDao {
 
     private ConnectionMaker connectionMaker;
 
-    public void setDataSource(DataSource dataSource){
+    public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public void setConnectionMaker(ConnectionMaker connectionMaker){
+    public void setConnectionMaker(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
+
     private Connection c;
     private User user;
 
-    public void add(User user) throws SQLException {
-        StatementStrategy st = new AddStatement(user);
-        jdbcContextWithStatementStrategy(st);
+    public void add(final User user) throws SQLException {
+
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                return ps;
+
+            }
+        });
     }
 
     public User get(String id) throws SQLException {
         Connection c = dataSource.getConnection();
 
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id =?");
+        PreparedStatement ps = c.prepareStatement("select * from users where id =?");
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
 
         User user = null;
-        if(rs.next()) {
+        if (rs.next()) {
             user = new User();
             user.setId(rs.getString("id"));
             user.setName(rs.getString("name"));
@@ -49,17 +59,21 @@ public class UserDao {
         ps.close();
         c.close();
 
-        if(user == null) throw new EmptyResultDataAccessException(1);
+        if (user == null) throw new EmptyResultDataAccessException(1);
 
         return user;
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                return c.prepareStatement("delete from users");
+            }
+        });
     }
 
-    public int getCount() throws SQLException{
+    public int getCount() throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -72,37 +86,61 @@ public class UserDao {
             rs = ps.executeQuery();
             rs.next();
             return rs.getInt(1);
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw e;
         } finally {
-            if(rs != null){ try { rs.close(); } catch (SQLException e){} }
-            if(ps!= null) { try{ ps.close(); }catch (SQLException e){} }
-            if(c!= null){ try{ c.close(); } catch (SQLException e){} }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
 
     public class SimpleConnectionMaker {
-        public Connection makeNewConnection() throws SQLException, ClassNotFoundException{
+        public Connection makeNewConnection() throws SQLException, ClassNotFoundException {
             Class.forName("com.mysql.jdbc.Driver");
-            java.sql.Connection c = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/study", "dragon", "1234");
+            java.sql.Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/study", "dragon", "1234");
             return c;
         }
     }
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
 
-        try{
+        try {
             c = dataSource.getConnection();
             ps = stmt.makePreparedStatement(c);
             ps.executeUpdate();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw e;
         } finally {
-            if(ps != null){ try {ps.close();} catch (SQLException e){} }
-            if(c != null){ try {c.close();} catch (SQLException e){} }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
 }
