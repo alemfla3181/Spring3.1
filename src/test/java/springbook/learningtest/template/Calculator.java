5d9368e262d693854c295092c54571a1bf78bed2
Dragon <alemfla3181@naver.com>
2022-10-02 16:50:52 +0900

    spring3.1 3장 템플릿 #숫자 합 계산 테스트

---
+++ src/test/java/springbook/learningtest/template/Calculator.java
@@ -0,0 +1,23 @@
+package springbook.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+    public Integer calcSum(String filepath) throws IOException{
+        // 한 줄씩 읽기 편하게 BufferedReader로 파일을 가져옴
+        BufferedReader br = new BufferedReader(new FileReader(filepath));
+        Integer sum = 0;
+        String line = null;
+
+        // 마지막 라인까지 한 줄씩 읽어가면서 숫자를 더함
+        while((line = br.readLine()) != null){
+            sum += Integer.valueOf(line);
+        }
+
+        // 한번 연 파일은 반드시 닫아야 함
+        br.close();
+        return sum;
+    }
+}
