e54afb5ab0644837006c7d984424571f051a4858
Dragon <alemfla3181@naver.com>
2022-10-02 17:29:42 +0900

    spring3.1 3장 템플릿 #제네릭스를 이용한 콜백 인터페이스

--- src/test/java/springbook/learningtest/template/Calculator.java
+++ src/test/java/springbook/learningtest/template/Calculator.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        LineCallback sumCallback = new LineCallback() {
+        LineCallback<Integer> sumCallback = new LineCallback<Integer>() {
             @Override
             public Integer doSomethingWithLine(String line, Integer value) {
                 return value + Integer.valueOf(line);
@@ -16,13 +16,23 @@ public class Calculator {
     }
 
     public Object calcMultiply(String filepath) throws IOException {
-        LineCallback multiplyCallback = new LineCallback() {
+        LineCallback<Integer> multiplyCallback = new LineCallback<Integer>() {
             @Override
             public Integer doSomethingWithLine(String line, Integer value) {
                 return value * Integer.valueOf(line);
             }
         };
         return lineReadTemplate(filepath, multiplyCallback, 1);
+    }
+
+    public String concatenate(String filepath) throws IOException{
+        LineCallback<String> concatenateCallback = new LineCallback<String>() {
+            @Override
+            public String doSomethingWithLine(String line, String value) {
+                return value + line;
+            }
+        };
+        return lineReadTemplate(filepath, concatenateCallback, "");
     }
 
     public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
@@ -45,11 +55,11 @@ public class Calculator {
         }
     }
 
-    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException{
+    public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException{
         BufferedReader br = null;
         try{
             br = new BufferedReader(new FileReader(filepath));
-            Integer res = initVal;
+            T res = initVal;
             String line = null;
             while((line = br.readLine()) != null){
                 res = callback.doSomethingWithLine(line, res);
