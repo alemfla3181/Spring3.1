fe972c4829f634564b85674ae676065d44ff7749
Dragon <alemfla3181@naver.com>
2022-10-02 16:55:05 +0900

    spring3.1 3장 템플릿 # try/catch 적용 테스트

--- src/test/java/springbook/learningtest/template/Calculator.java
+++ src/test/java/springbook/learningtest/template/Calculator.java
@@ -7,17 +7,30 @@ import java.io.IOException;
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException{
         // 한 줄씩 읽기 편하게 BufferedReader로 파일을 가져옴
-        BufferedReader br = new BufferedReader(new FileReader(filepath));
-        Integer sum = 0;
-        String line = null;
+        BufferedReader br = null;
 
-        // 마지막 라인까지 한 줄씩 읽어가면서 숫자를 더함
-        while((line = br.readLine()) != null){
-            sum += Integer.valueOf(line);
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer sum = 0;
+            String line = null;
+            // 마지막 라인까지 한 줄씩 읽어가면서 숫자를 더함
+            while ((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            return sum;
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            // 한번 연 파일은 반드시 닫아야 함
+            if(br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
-
-        // 한번 연 파일은 반드시 닫아야 함
-        br.close();
-        return sum;
     }
 }
