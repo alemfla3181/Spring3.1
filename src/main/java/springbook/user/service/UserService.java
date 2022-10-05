9069ecaf67b3d430b90650155d2999a3c4232996
Dragon <alemfla3181@naver.com>
2022-10-05 14:12:54 +0900

    spring3.1 5장 서비스 추상화 #인터페이스 생성

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -6,7 +6,7 @@ import springbook.user.domain.User;
 
 import java.util.List;
 
-public class UserService {
+public class UserService implements UserLevelUpgradePolicy{
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECCOMEND_FOR_GOLD = 30;
     UserDao userDao;
@@ -24,12 +24,12 @@ public class UserService {
         }
     }
 
-    private void upgradeLevel(User user) {
+    public void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
     }
 
-    private boolean canUpgradeLevel(User user) {
+    public boolean canUpgradeLevel(User user) {
         Level currentLevel = user.getLevel();
         switch (currentLevel){
             case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
