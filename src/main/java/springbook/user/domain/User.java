a6a7209db29ce2e45e1e3bc6fccb6c332bc8529c
Dragon <alemfla3181@naver.com>
2022-10-06 15:12:10 +0900

    spring3.1 5장 서비스 추상화 #email 필드 추가

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -7,23 +7,19 @@ public class User {
     Level level;
     int login;
     int recommend;
+    String email;
 
     public User() {
     }
 
-    public User(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public User(String id, String name, String password, Level level, int login, int recommend) {
+    public User(String id, String name, String password, Level level, int login, int recommend, String email) {
         this.id = id;
         this.name = name;
         this.password = password;
         this.level = level;
         this.login = login;
         this.recommend = recommend;
+        this.email = email;
     }
 
     public void upgradeLevel(){
@@ -82,6 +78,10 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
 }
 
 
