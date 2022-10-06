cafa4389bc7ffa0166c82eda20e2f19e8bd8147f
Dragon <alemfla3181@naver.com>
2022-10-06 15:12:26 +0900

    spring3.1 5장 서비스 추상화 #email 필드 추가

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -28,13 +28,15 @@ public class UserDaoJdbc implements UserDao {
             user.setLevel(Level.valueOf((rs.getInt("level"))));
             user.setLogin(rs.getInt("login"));
             user.setRecommend(rs.getInt("recommend"));
+            user.setEmail(rs.getString("email"));
             return user;
         }
     };
 
     public void add(final User user) throws DuplicateUserIdException {
-        this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend) values (?,?,?,?,?,?)",
-            user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
+        this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend, email) values (?,?,?,?,?,?,?)",
+            user.getId(), user.getName(), user.getPassword(),
+            user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail());
     }
 
     public User get(String id) {
@@ -52,8 +54,9 @@ public class UserDaoJdbc implements UserDao {
     @Override
     public void update(User user) {
         this.jdbcTemplate.update(
-            "update users set name = ?, password = ?, level = ?, login =?, recommend =? where id =? ",
-            user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getId());
+            "update users set name = ?, password = ?, level = ?, login =?, recommend =?, email =? where id =? ",
+            user.getName(), user.getPassword(), user.getLevel().intValue(),
+            user.getLogin(), user.getRecommend(), user.getEmail(),  user.getId());
     }
 
     public List<User> getAll(){
