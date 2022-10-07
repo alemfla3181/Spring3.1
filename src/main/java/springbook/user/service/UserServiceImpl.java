51803f7c9f4e4a47c0538e87ba23d82852c24682
Dragon <alemfla3181@naver.com>
2022-10-07 13:33:57 +0900

    spring3.1 6장 AOP #UserService 구현 클래스

--- src/main/java/springbook/user/service/UserServiceImpl.java
+++ src/main/java/springbook/user/service/UserServiceImpl.java
@@ -1,57 +1,32 @@
 package springbook.user.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
-import javax.mail.Session;
-import javax.sql.DataSource;
-import java.sql.Connection;
 import java.util.List;
-import java.util.Properties;
 
 public class UserServiceImpl implements UserService{
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECCOMEND_FOR_GOLD = 30;
-    @Autowired
-    private DataSource dataSource;
-
-    private PlatformTransactionManager transactionManager;
-    private MailSender mailSender;
 
     UserDao userDao;
-
-    public void setDataSource(DataSource dataSource) { this.dataSource = dataSource; }
+    MailSender mailSender;
 
     public void setUserDao(UserDao userDao){
         this.userDao = userDao;
     }
-
-    public void setTransactionManager(PlatformTransactionManager transactionManager) { this.transactionManager = transactionManager; }
-
     public void setMailSender(MailSender mailSender) { this.mailSender = mailSender; }
 
     public void upgradeLevels(){
-        TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
-
-        try {
-            upgradeLevelsInternal();
-            // 정상적으로 작업을 마치면 트랜잭션 커밋
-            this.transactionManager.commit(status);
-        } catch (RuntimeException e) {
-            // 예외 발생 시 롤백
-            this.transactionManager.rollback(status);
-            throw e;
+        List<User> users = userDao.getAll();
+        for(User user : users){
+            if(canUpgradeLevel(user)){
+                upgradeLevel(user);
+            }
         }
     }
 
