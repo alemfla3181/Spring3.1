77de7c022d7164846238a6b0f867e4d0d8aae880
Dragon <alemfla3181@naver.com>
2022-10-08 14:29:03 +0900

    spring3.1 6장 AOP #팩토리 빈 테스트

---
+++ src/test/java/springbook/learningtest/jdk/FactoryBeanTest.java
@@ -0,0 +1,32 @@
+package springbook.learningtest.jdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.learningtest.spring.factorybean.Message;
+import springbook.learningtest.spring.factorybean.MessageFactoryBean;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/FactoryBeanTest-context.xml")
+public class FactoryBeanTest {
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    public void getMessageFromFactoryBean(){
+        Object message = context.getBean("message");
+        assertThat(message).isInstanceOf(Message.class);
+        assertThat(((Message)message).getText()).isEqualTo("Factory Bean");
+    }
+
+    @Test
+    public void getFactoryBean() throws Exception{
+        Object factory = context.getBean("&message");
+        assertThat(factory).isInstanceOf(MessageFactoryBean.class);
+    }
+}
